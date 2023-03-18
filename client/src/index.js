49afb0e3981b08a11e9d7fcbe9eb20b0451b6b86
Kinga <danielewicz.kinga@gmail.com>
2023-03-18 09:46:11 +0100

    dark mode setup

--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { ConfigureStore } from "@reduxjs/toolkit";
+import globalReducer from "state";
+import { Provider } from "react-redux";
+
+const store = ConfigureStore({
+  reducer: {
+    global: globalReducer,
+  },
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
