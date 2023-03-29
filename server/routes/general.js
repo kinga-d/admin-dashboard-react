256e309184b172a04dbbcad70572f65e71824360
Kinga <danielewicz.kinga@gmail.com>
2023-03-29 13:22:34 +0200

    bug fixed

--- server/routes/general.js
+++ server/routes/general.js
@@ -3,6 +3,6 @@ import { getUser } from "../controllers/general.js";
 
 const router = express.Router();
 
-router.get("/user/:id, getUser");
+router.get("/user/:id", getUser);
 
 export default router;
